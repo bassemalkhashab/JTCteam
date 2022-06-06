93aaf91c51bc0aeabd0061b38afebe5848e1a6ab
bassemalkhashab <bassemalkhashab@gmail.com>
2022-06-06 10:09:32 +0200

    Add loading icon to home

--- public/js/app.js
+++ public/js/app.js
@@ -5693,13 +5693,62 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
 
 
 function Home() {
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      loaded1 = _useState2[0],
+      setLoaded1 = _useState2[1];
+
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false),
+      _useState4 = _slicedToArray(_useState3, 2),
+      loaded2 = _useState4[0],
+      setLoaded2 = _useState4[1];
+
+  var _useState5 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false),
+      _useState6 = _slicedToArray(_useState5, 2),
+      loaded3 = _useState6[0],
+      setLoaded3 = _useState6[1];
+
+  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
+    var image1 = new Image();
+    image1.src = '/storage/sofa.png';
+
+    image1.onload = function () {
+      setLoaded1(true);
+    };
+
+    var image2 = new Image();
+    image2.src = '/storage/plate.png';
+
+    image2.onload = function () {
+      setLoaded2(true);
+    };
+
+    var image3 = new Image();
+    image3.src = '/storage/clothes.png';
+
+    image3.onload = function () {
+      setLoaded3(true);
+    };
+  }, []);
+  return loaded1 && loaded2 && loaded3 && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
     className: "container",
     children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h1", {
       className: "mt-5 fw-bolder",
@@ -5714,7 +5763,7 @@ function Home() {
         to: "/service/residential",
         className: "option",
         children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("img", {
-          src: "storage/sofa.png",
+          src: "/storage/sofa.png",
           alt: "Sofa",
           height: 300
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h5", {
@@ -5724,7 +5773,7 @@ function Home() {
         to: "/service/commercial",
         className: "option",
         children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("img", {
-          src: "storage/plate.png",
+          src: "/storage/plate.png",
           alt: "Sofa",
           height: 300
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h5", {
@@ -5734,7 +5783,7 @@ function Home() {
         to: "/service/clothes",
         className: "option",
         children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("img", {
-          src: "storage/clothes.png",
+          src: "/storage/clothes.png",
           alt: "Sofa",
           height: 300
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h5", {
@@ -5742,6 +5791,12 @@ function Home() {
         })]
       })]
     })]
+  }) || /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+    className: "container d-flex justify-content-center align-items-center vh-100",
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+      "class": "lds-ellipsis",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {})]
+    })
   });
 }
 
--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom';
 
 function Home() {
-  return (
+    const [loaded1, setLoaded1]=useState(false);
+    const [loaded2, setLoaded2]=useState(false);
+    const [loaded3, setLoaded3]=useState(false);
+
+    useEffect(()=>{
+        const image1 = new Image();
+        image1.src='/storage/sofa.png';
+        image1.onload = () => {
+            setLoaded1(true);
+        };
+        const image2 = new Image();
+        image2.src='/storage/plate.png';
+        image2.onload = () => {
+            setLoaded2(true);
+        };
+        const image3 = new Image();
+        image3.src='/storage/clothes.png';
+        image3.onload = () => {
+            setLoaded3(true);
+          };
+    },[]);
+
+  return loaded1&&loaded2&&loaded3&&(
       <div className="container">
             <h1 className='mt-5 fw-bolder'>We customize cleaning for you</h1>
             <h5 className='mt-3'>Professional and experienced cleaning service that delivers the results you expect. We pride ourselves on providing reliable, fast and detailed results.</h5>
             <div className="d-flex justify-content-between mt-5" id='options'>
                 <Link to='/service/residential' className="option">
-                    <img src="storage/sofa.png" alt="Sofa" height={300} />
+                    <img src="/storage/sofa.png" alt="Sofa" height={300} />
                     <h5>Residential</h5>
                 </Link>
                 <Link to='/service/commercial' className="option">
-                    <img src="storage/plate.png" alt="Sofa" height={300} />
+                    <img src="/storage/plate.png" alt="Sofa" height={300}/>
                     <h5>Commercial</h5>
                 </Link>
                 <Link to='/service/clothes' className="option">
-                    <img src="storage/clothes.png" alt="Sofa" height={300} />
+                    <img src="/storage/clothes.png" alt="Sofa" height={300} />
                     <h5>Clothes</h5>
                 </Link>
             </div>
       </div>
-  )
+  )||(<div className='container d-flex justify-content-center align-items-center vh-100'><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>)
 }
 
 export default Home
